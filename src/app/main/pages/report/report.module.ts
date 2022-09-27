e0a3e7fc6045bad30a86733f36539ab10d1de593
phasamon-p <phasamon.p@gmail.com>
2022-09-27 23:31:09 +0700

    update daily-report page

--- src/app/main/pages/report/report.module.ts
+++ src/app/main/pages/report/report.module.ts
@@ -1,11 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AuthGuard } from 'app/auth/helpers';
-import { CoreCommonModule } from '@core/common.module';
+import { CsvModule } from '@ctrl/ngx-csv';
+
 import { ContentHeaderModule } from 'app/layout/components/content-header/content-header.module';
-import { DailyReportComponent } from './daily-report/daily-report.component';
+
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { Ng2FlatpickrModule } from 'ng2-flatpickr';
+
+import { CoreCommonModule } from '@core/common.module';
+import { CoreDirectivesModule } from '@core/directives/directives';
+import { CorePipesModule } from '@core/pipes/pipes.module';
+import { CoreSidebarModule } from '@core/components';
+
+import { InvoiceListService } from 'app/main/apps/invoice/invoice-list/invoice-list.service';
+import { InvoiceModule } from 'app/main/apps/invoice/invoice.module';
+
+
+import { DailyReportComponent } from 'app/main/pages/report/daily-report/daily-report.component';
+import { DailyReportService } from 'app/main/pages/report/daily-report/daily-report.service';
+
 import { JobSummaryReportComponent } from './job-summary-report/job-summary-report.component';
 import { ReportFromMachineComponent } from './report-from-machine/report-from-machine.component';
 
@@ -15,9 +34,9 @@ const routes: Routes = [
     component: DailyReportComponent,
     canActivate: [AuthGuard],
     resolve: {
-      // profile: ProfileService
+      uls: DailyReportService
     },
-    // data: { animation: 'list' }
+    data: { animation: 'DailyReportComponent' }
   },
   {
     path: 'jobsummary-report',
@@ -50,7 +69,17 @@ const routes: Routes = [
     RouterModule.forChild(routes), 
     NgbModule, 
     CoreCommonModule, 
-    ContentHeaderModule
+    ContentHeaderModule,
+    FormsModule,
+    NgbModule,
+    NgSelectModule,
+    Ng2FlatpickrModule,
+    NgxDatatableModule,
+    CorePipesModule,
+    CoreDirectivesModule,
+    InvoiceModule,
+    CoreSidebarModule,
+    CsvModule
   ],
   // providers: [ProfileService]
 })
