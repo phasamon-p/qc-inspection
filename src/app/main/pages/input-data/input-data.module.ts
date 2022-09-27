4ca8c1a4232c8c77f5a0d71e0b01377d94c6e539
phasamon-p <phasamon.p@gmail.com>
2022-09-27 15:57:59 +0700

    update input-data page

--- src/app/main/pages/input-data/input-data.module.ts
+++ src/app/main/pages/input-data/input-data.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AuthGuard } from 'app/auth/helpers';
 import { CoreCommonModule } from '@core/common.module';
+import { CardSnippetModule } from '@core/components/card-snippet/card-snippet.module';
 import { ContentHeaderModule } from 'app/layout/components/content-header/content-header.module';
 
 import { InputDataComponent } from './input-data/input-data.component';
@@ -26,11 +28,14 @@ const routes: Routes = [
     InputDataComponent
   ],
   imports: [
-    CommonModule, 
-    RouterModule.forChild(routes), 
-    NgbModule, 
-    CoreCommonModule, 
-    ContentHeaderModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    CoreCommonModule,
+    NgbModule,
+    ContentHeaderModule,
+    CardSnippetModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   // providers: [ProfileService]
 })
--- src/app/main/pages/input-data/input-data/input-data.component.ts
+++ src/app/main/pages/input-data/input-data/input-data.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
+
+import * as snippet from 'app/main/forms/form-validation/form-validation.snippetcode';
+import { MustMatch } from './_helpers/must-match.validator';
+
 
 @Component({
   selector: 'app-input-data',
@@ -6,10 +11,102 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-data.component.scss']
 })
 export class InputDataComponent implements OnInit {
+  // public
+  public contentHeader: object;
 
-  constructor() { }
+  public _snippetCodeTDsimpleValidation = snippet.snippetCodeTDsimpleValidation;
+  public _snippetCodeTDMultiRuleValidation = snippet.snippetCodeTDMultiRuleValidation;
+  public _snippetCodeInputValidation = snippet.snippetCodeInputValidation;
+  public _snippetCodeReactiveForms = snippet.snippetCodeReactiveForms;
 
-  ngOnInit(): void {
+  public TDNameVar;
+  public TDEmailVar;
+
+  public ReactiveUserDetailsForm: UntypedFormGroup;
+  public ReactiveUDFormSubmitted = false;
+
+  public currentRow;
+
+  // Reactive User Details form data
+  public UDForm = {
+    userName: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confPassword: '',
+    age: '',
+    phoneNumber: ''
+  };
+
+  constructor(private formBuilder: UntypedFormBuilder) {}
+
+  // getter for easy access to form fields
+  get ReactiveUDForm() {
+    return this.ReactiveUserDetailsForm.controls;
+  }
+
+  ReactiveUDFormOnSubmit() {
+    this.ReactiveUDFormSubmitted = true;
+
+    // stop here if form is invalid
+    if (this.ReactiveUserDetailsForm.invalid) {
+      return;
+    }
+    console.log(this.ReactiveUserDetailsForm.value);
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.ReactiveUserDetailsForm.value));
+  }
+
+  // Lifecycle Hooks
+  // -----------------------------------------------------------------------------------------------------
+
+  /**
+   * On init
+   */
+  ngOnInit() {
+    // content header
+    this.contentHeader = {
+      headerTitle: 'Form Validation',
+      actionButton: true,
+      breadcrumb: {
+        type: '',
+        links: [
+          {
+            name: 'Home',
+            isLink: true,
+            link: '/'
+          },
+          {
+            name: 'Forms',
+            isLink: true,
+            link: '/'
+          },
+          {
+            name: 'Form Validation',
+            isLink: false
+          }
+        ]
+      }
+    };
+
+    // Reactive form initialization
+    this.ReactiveUserDetailsForm = this.formBuilder.group(
+      {
+        userName: ['', Validators.required],
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confPassword: ['', [Validators.required, Validators.minLength(6)]],
+        country: ['', [Validators.required]],
+        language: ['', [Validators.required]],
+        age: ['', [Validators.required]],
+        phoneNumber: ['', [Validators.required]]
+      },
+      {
+        validator: MustMatch('password', 'confPassword')
+      }
+    );
   }
 
 }
