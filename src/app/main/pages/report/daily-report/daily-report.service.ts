04d27bab1d4fa6b5b9f832d073f55f4cb57a739c
phasamon-p <phasamon.p@gmail.com>
2022-09-27 23:32:12 +0700

    update daily-report page

---
+++ src/app/main/pages/report/daily-report/daily-report.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DailyReportService implements Resolve<any>  {
+  public rows: any;
+  public onReportListChanged: BehaviorSubject<any>;
+
+  /**
+   * Constructor
+   *
+   * @param {HttpClient} _httpClient
+   */
+  constructor(private _httpClient: HttpClient) {
+    // Set the defaults
+    this.onReportListChanged = new BehaviorSubject({});
+    console.log(this.onReportListChanged)
+  }
+
+  /**
+   * Resolver
+   *
+   * @param {ActivatedRouteSnapshot} route
+   * @param {RouterStateSnapshot} state
+   * @returns {Observable<any> | Promise<any> | any}
+   */
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
+    return new Promise<void>((resolve, reject) => {
+      Promise.all([this.getDataTableRows()]).then(() => {
+        resolve();
+      }, reject);
+    });
+  }
+
+  /**
+   * Get rows
+   */
+  getDataTableRows(): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+      this._httpClient.get('api/users-data').subscribe((response: any) => {
+        this.rows = response;
+        this.onReportListChanged.next(this.rows);
+        resolve(this.rows);
+      }, reject);
+    });
+  }
+}
