8fc8192fa5c27691d8e2743d206a672bdddae046
phasamon-p <phasamon.p@gmail.com>
2022-09-30 23:54:24 +0700

    Update

---
+++ src/app/main/pages/report/job-summary-report/job-summary-report.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobSummaryReportService {
+  private _jsonURL = 'assets/json/job-summary-report.json';
+  public rows: any;
+  public onReportListChanged: Subject<any>;
+
+  /**
+   * Constructor
+   *
+   * @param {HttpClient} _httpClient
+   */
+  constructor(private _httpClient: HttpClient) {
+    // Set the defaults
+    // this.onReportListChanged = new BehaviorSubject({});
+    this.onReportListChanged = new Subject();
+  }
+
+  /**
+   * Resolver
+   *
+   * @param {ActivatedRouteSnapshot} route
+   * @param {RouterStateSnapshot} state
+   * @returns {Observable<any> | Promise<any> | any}
+   */
+
+   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
+    return this._httpClient.get(this._jsonURL).subscribe(data => {
+      this.onReportListChanged.next(data);
+     });
+  }
+}
